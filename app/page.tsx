4c210f2e9c19aa7d3e81974ff7a51bdfd2f14415
Zeeshan Bhati <zeeshanbhati99@gmail.com>
2024-04-13 04:43:58 +0530

    Minor Fixes For Editor

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ import { useTheme } from "next-themes";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-yaml";
 import "ace-builds/src-noconflict/theme-dracula";
-import 'ace-builds/src-noconflict/theme-github'
+import "ace-builds/src-noconflict/theme-github";
 import Header from "@/components/DockerFormComponents/Header";
 
 const initialData: IDockerForm = {
@@ -30,7 +30,7 @@ const initialData: IDockerForm = {
 };
 
 export default function Home() {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   const methods = useForm({ defaultValues: initialData });
   const { register, control } = methods;
   const { fields, remove, append } = useFieldArray({
@@ -38,11 +38,6 @@ export default function Home() {
     name: "services",
   });
   const [yamlData, setYamlData] = useState<string>();
-  
-
-
-
-
 
   const stringifyToYaml = (obj: any) => {
     try {
@@ -53,8 +48,6 @@ export default function Home() {
     }
   };
 
-
-
   const onSubmit = (data: IDockerForm) => {
     //console.log(data);
     //console.log("=========================");
@@ -62,8 +55,6 @@ export default function Home() {
     stringifyToYaml(res);
     console.log(res);
   };
-
- 
 
   const appendService = () => {
     append({
@@ -79,83 +70,77 @@ export default function Home() {
 
   return (
     <main className="h-screen overflow-hidden">
-      <Header/>
+      <Header />
       <div className="flex flex-row">
+        <div className="grid w-2/6 min-w-sm items-start gap-1.5 dark:bg-gray-900 b-stone-200 rounded-r-md">
+          <ScrollArea className="h-screen pt-8 px-12 relative">
+            <FormProvider {...methods}>
+              <form onSubmit={methods.handleSubmit(onSubmit)}>
+                <div className="mt-2 space-y-4">
+                  <div className="space-y-2">
+                    <Label className="">Version</Label>
+                    <Input
+                      id="version"
+                      placeholder="version"
+                      {...methods.register(`version`)}
+                    />
+                  </div>
 
-      <div className="grid w-2/6 min-w-sm items-start gap-1.5 dark:bg-gray-900 b-stone-200 rounded-r-md">
-        <ScrollArea className="h-screen pt-8 px-12 relative" >
-          <FormProvider {...methods}>
-            <form onSubmit={methods.handleSubmit(onSubmit)} >
-              <div className="mt-2 space-y-4">
-                <div className="space-y-2">
-                  <Label className="">Version</Label>
-                  <Input
-                    id="version"
-                    placeholder="version"
-                    {...methods.register(`version`)}
-                  />
+                  <h1 className="py-2"> Service Form </h1>
+
+                  {fields.map((service: any, index: number) => (
+                    <Accordion
+                      type="single"
+                      collapsible
+                      className="w-full"
+                      key={service.id}
+                    >
+                      <AccordionItem value="item-1">
+                        <AccordionTrigger>
+                          <div className="w-full px-2 flex items-center justify-between">
+                            <Label className="text-lg">Service</Label>
+                            <Button
+                              size="icon"
+                              variant={"ghost"}
+                              onClick={() => removeService(index)}
+                            >
+                              <CircleX color={"#2580F7"} />
+                            </Button>
+                          </div>
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          <div key={service.id} className="space-y-2 px-2">
+                            <ServiceForm index={index} />
+                          </div>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  ))}
+                  <div className="pt-4">
+                    <Button className="" type="button" onClick={appendService}>
+                      Add
+                    </Button>
+                  </div>
                 </div>
-
-                <h1 className="py-2"> Service Form </h1>
-
-                {fields.map((service: any, index: number) => (
-                  <Accordion
-                    type="single"
-                    collapsible
-                    className="w-full"
-                    key={service.id}
-                  >
-                    <AccordionItem value="item-1">
-                      <AccordionTrigger>
-                        <div className="w-full px-2 flex items-center justify-between">
-                          <Label className="text-lg">Service</Label>
-                          <Button
-                            size="icon"
-                            variant={"ghost"}
-                            onClick={() => removeService(index)}
-                          >
-                            <CircleX color={"#2580F7"} />
-                          </Button>
-                        </div>
-                      </AccordionTrigger>
-                      <AccordionContent>
-                        <div key={service.id} className="space-y-2 px-2">
-                          <ServiceForm index={index} />
-                        </div>
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                ))}
-                <div className="pt-10">
-                <Button className="" type="button" onClick={appendService}>
-                  Add
-                </Button>
+                <div className="pb-32">
+                  <Button type="submit" className="mt-8">
+                    Submit
+                  </Button>
                 </div>
-               
-              </div>
-              <div className="pb-32">
-              <Button type="submit" className="mt-8">
-                Submit
-              </Button>
-
-              </div>
-              
-            </form>
-          </FormProvider>
-        </ScrollArea>
+              </form>
+            </FormProvider>
+          </ScrollArea>
+        </div>
+        <div className="bg-grey-900 text-lg font-semibold flex-1">
+          <AceEditor
+            mode="yaml"
+            theme={theme === "dark" ? "dracula" : "github"} // Choose your preferred theme
+            value={yamlData}
+            readOnly={true} // Make the editor read-only if needed
+            style={{ width: "100%", height: "100%", fontSize: "16px" }} // Adjust the size as needed
+          />
+        </div>
       </div>
-      <div className="bg-grey-900 text-lg font-semibold flex-1">
-        <AceEditor
-          mode="yaml"
-          theme={theme === 'dark' ? "dracula" : "github"} // Choose your preferred theme
-          value={yamlData}
-          readOnly={true} // Make the editor read-only if needed
-          style={{ width: "100%", height: "100%" }} // Adjust the size as needed
-        />
-      </div>
-        
-      </div>
-     
     </main>
   );
 }
